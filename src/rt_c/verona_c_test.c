2ab6c3711bac01d6f460945c6017ec5aa3c255ee
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2020-01-17 21:14:16 -0800

    Fix indentation in src/rt_c/verona_c_test.c

--- src/rt_c/verona_c_test.c
+++ src/rt_c/verona_c_test.c
@@ -8,29 +8,29 @@
 
 struct BankAccount {
   void* _rt_cown_overhead[8];
-	uint64_t balance;
+  uint64_t balance;
 };
 typedef struct BankAccount BankAccount;
 void BankAccount_Trace(BankAccount* o, RTObjectStack* st) { return; }
 RTDescriptor BankAccount_Desc = {
-	sizeof(BankAccount),
-	(RTDescriptorTraceFunction)BankAccount_Trace,
-	NULL,
-	NULL,
-	NULL
+  sizeof(BankAccount),
+  (RTDescriptorTraceFunction)BankAccount_Trace,
+  NULL,
+  NULL,
+  NULL
 };
 
 ///
 // BankAccount_create - create a cown[BankAccount] with an opening balance.
 
 BankAccount* BankAccount_create(uint64_t opening_balance) {
-	BankAccount* self =
-		(BankAccount*)RTCown_alloc(&BankAccount_Desc);
+  BankAccount* self =
+    (BankAccount*)RTCown_alloc(&BankAccount_Desc);
 
-	self->balance = opening_balance;
+  self->balance = opening_balance;
 
-	printf("opening balance: %lld\n", self->balance);
-	return self;
+  printf("opening balance: %lld\n", self->balance);
+  return self;
 }
 
 ///
@@ -43,26 +43,26 @@ struct BankAccount_add_Action {
 };
 typedef struct BankAccount_add_Action BankAccount_add_Action;
 void BankAccount_add_ActionTrace(BankAccount_add_Action* a, RTObjectStack* st) {
-	RTObjectStack_push(st, (RTObject*)a->self);
+  RTObjectStack_push(st, (RTObject*)a->self);
 }
 void BankAccount_add_fn(BankAccount_add_Action* action);
 RTActionDescriptor BankAccount_add_ActionDesc = {
-	sizeof(BankAccount_add_Action),
-	(RTActionFunction)BankAccount_add_fn,
-	(RTActionTraceFunction)BankAccount_add_ActionTrace
+  sizeof(BankAccount_add_Action),
+  (RTActionFunction)BankAccount_add_fn,
+  (RTActionTraceFunction)BankAccount_add_ActionTrace
 };
 void BankAccount_add(BankAccount* self, uint64_t amount) {
-	BankAccount_add_Action* action =
-		(BankAccount_add_Action*)RTAction_new(&BankAccount_add_ActionDesc);
-	action->self = self;
-	action->amount = amount;
+  BankAccount_add_Action* action =
+    (BankAccount_add_Action*)RTAction_new(&BankAccount_add_ActionDesc);
+  action->self = self;
+  action->amount = amount;
 
-	RTAction_schedule((RTAction*)action, (RTCown**)&self, 1);
+  RTAction_schedule((RTAction*)action, (RTCown**)&self, 1);
 }
 void BankAccount_add_fn(BankAccount_add_Action* action) {
-	// All the above is just boilerplate; this is where the action is implemented.
-	action->self->balance = action->self->balance + action->amount;
-	printf("new balance: %lld\n", action->self->balance);
+  // All the above is just boilerplate; this is where the action is implemented.
+  action->self->balance = action->self->balance + action->amount;
+  printf("new balance: %lld\n", action->self->balance);
 }
 
 ///
@@ -72,16 +72,16 @@ int main(int argc, const char* argv[])
 {
   RTScheduler_init(1);
   RTScheduler_set_detect_leaks(true);
-	printf("RTScheduler setup complete!\n");
+  printf("RTScheduler setup complete!\n");
 
-	BankAccount* account = BankAccount_create(1000);
-	// RTCown_acquire((RTCown*)account);
-	BankAccount_add(account, 200);
-	BankAccount_add(account, 500);
-	BankAccount_add(account, 1500);
-	RTCown_release((RTCown*)account);
+  BankAccount* account = BankAccount_create(1000);
+  // RTCown_acquire((RTCown*)account);
+  BankAccount_add(account, 200);
+  BankAccount_add(account, 500);
+  BankAccount_add(account, 1500);
+  RTCown_release((RTCown*)account);
 
-	printf("RTScheduler running...\n");
+  printf("RTScheduler running...\n");
   RTScheduler_run();
-	printf("RTScheduler finished!\n");
+  printf("RTScheduler finished!\n");
 }
